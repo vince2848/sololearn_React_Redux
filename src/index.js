6bdf42b5e6d7d3a36575db3dfe43bdf1d0c956be
Vincent <vince2848@gmail.com>
2022-05-17 19:47:30 -0600

    Lesson 6.1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const name = "David";
+const el = <p>Hello, {name}</p>;
+
 root.render(
-  <h1>Hello, React</h1>
+  el
 );
 
 // If you want to start measuring performance in your app, pass a function
