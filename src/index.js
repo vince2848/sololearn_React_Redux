c67eb192c79eb66edb597888f013c46f81f25b57
Vincent <vince2848@gmail.com>
2022-05-24 20:10:16 -0600

    Lesson 13.1 Handling user input

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-class Counter extends React.Component {
-  state = {
-    counter: 0
+function Converter() {
+  const [km, setKm] = useState(0);
+
+  function handleChange(e) {
+    setKm(e.target.value);
+  }
+  function convert(km) {
+    return (km/1.609).toFixed(2);
   }
 
-  increment = () => {
-    this.setState({
-      counter: this.state.counter+1
-    });
-  }
-
-  componentDidMount() {
-    this.setState({counter: 42});
-  }
-
-  render() {
-    return <div>
-      <p>{this.state.counter}</p>
-      <button onClick={this.increment}>Increment</button>
-    </div>;
-  }
+  return <div>
+    <input type="text" value={km} onChange={handleChange} />
+    <p> {km} km is {convert(km)} miles</p>
+  </div>
 }
-const el =<Counter/>
+
+
+const el =<Converter/>
 root.render(el)
 
 // If you want to start measuring performance in your app, pass a function
